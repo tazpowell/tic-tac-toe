452b7de8d7efbc184a7c4eaa68293be4d4091e14
tazpowell <hellotazuko@gmail.com>
2018-06-09 11:46:32 -0400

    Add CURL scripts for show and index
    - Test CURL scripts for:
    -- show a game by id
    -- show all games for user (index)
    -- show all 'over' games for user (over-true)
    -- show all games not 'over' for user (over-false)

---
+++ curl-scripts/game/show.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games/${ID}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data ''
+
+echo
